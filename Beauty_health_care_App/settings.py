19f397b408b87ac15707c7f37bba850e459e2b82
Your Name <you@example.com>
2023-07-22 17:10:12 +0200

    connect with posgresql

--- Beauty_health_care_App/settings.py
+++ Beauty_health_care_App/settings.py
@@ -44,7 +44,7 @@ INSTALLED_APPS = [
     'knox',
     'cart',
     'order',
-    'payment'
+    'rest_framework.authtoken'
 ]
 
 REST_FRAMEWORK = {
@@ -92,13 +92,22 @@ WSGI_APPLICATION = 'Beauty_health_care_App.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "postgres",
+        "USER": "postgres",
+        "PASSWORD": "shymaa99",
+        "HOST": "127.0.0.1",
+        "PORT": "5432",
     }
 }
-
 
 
 # Password validation
