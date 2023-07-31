9bf5701345fea68fbb3bba67f90a0239f79c54b4
Your Name <you@example.com>
2023-07-31 18:24:23 +0200

    updated database

--- Beauty_health_care_App/settings.py
+++ Beauty_health_care_App/settings.py
@@ -23,8 +23,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-w6t43%lj$6!6#pow#k7g%!ue02yzqs%s=g!6xw+igwtku)32fd'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
-ALLOWED_HOSTS = ['localhost','skinCareAPI.pythonanywhere.com']
+DEBUG = True
+#ALLOWED_HOSTS = ['localhost','skinCareAPI.pythonanywhere.com']
 
 ALLOWED_HOSTS=[]
 # Application definition
@@ -94,22 +94,22 @@ WSGI_APPLICATION = 'Beauty_health_care_App.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+DATABASES = {
+     'default': {
+         'ENGINE': 'django.db.backends.sqlite3',
+         'NAME': BASE_DIR / 'db.sqlite3',
+     }
+ }
 # DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
+#     "default": {
+#         "ENGINE": "django.db.backends.postgresql",
+#         "NAME": "postgres",
+#         "USER": "postgres",
+#         "PASSWORD": "shymaa99",
+#         "HOST": "127.0.0.1",
+#         "PORT": "5432",
 #     }
 # }
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": "postgres",
-        "USER": "postgres",
-        "PASSWORD": "shymaa99",
-        "HOST": "127.0.0.1",
-        "PORT": "5432",
-    }
-}
 
 
 # Password validation
